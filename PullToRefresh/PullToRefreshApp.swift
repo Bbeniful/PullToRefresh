e51b967a3ac0f044073b2b66c9ed7c40ca00daf4
Bbeniful <bbenikeszi@gmail.com>
2024-03-28 19:33:15 +0100

    Initial Commit

---
+++ PullToRefresh/PullToRefreshApp.swift
@@ -0,0 +1,17 @@
+//
+//  PullToRefreshApp.swift
+//  PullToRefresh
+//
+//  Created by Benjámin Szilágyi on 28/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct PullToRefreshApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
